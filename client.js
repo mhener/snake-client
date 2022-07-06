af3a16fa1fc3c57d93853f6f8d08e332138f4168
Mardin Hener <mardinhener14@gmail.com>
2022-07-06 21:49:24 +0000

    last step

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
-// establishes a connection with the game server
 const { on } = require("events");
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
+    IP,
+    PORT,
   },);
 
   // interpret incoming data as text
